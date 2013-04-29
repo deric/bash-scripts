83ccd746802353633754c898dd0dc576cb77a74d
Tomas Barton <barton.tomas@gmail.com>
2013-04-29 12:15:52 +0200

    relative path (doesn't matter where you put these scripts)

--- all.sh
+++ all.sh
@@ -1,4 +1,12 @@
-source ~/bash-scripts/terminal/git.sh
-source ~/bash-scripts/terminal/shell.sh
-source ~/bash-scripts/terminal/rails.sh
-source ~/bash-scripts/terminal/prompt.sh
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source ${DIR}/terminal/git.sh
+source ${DIR}/terminal/shell.sh
+source ${DIR}/terminal/rails.sh
+source ${DIR}/terminal/prompt.sh
