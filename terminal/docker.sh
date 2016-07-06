ec6efe4c9ff206322649abbdbf35d2cc16142a3e
Tomas Barton <barton.tomas@gmail.com>
2016-07-06 18:02:37 +0200

    dstop for stopping all docker containers

--- terminal/docker.sh
+++ terminal/docker.sh
@@ -7,3 +7,6 @@ drmi(){
   docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
   docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
+
+# stop all running containers
+alias dstop='docker stop $(docker ps -a -q)'
